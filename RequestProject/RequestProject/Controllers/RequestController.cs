806eb56b30470c89ebbb3476b8a301d706b9c47f
pasindu-osadha <pasiduosanda@gmail.com>
2022-06-16 14:53:19 +0530

    add basic request controller to get response from response project

---
+++ RequestProject/RequestProject/Controllers/RequestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult> MakeRequest()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://localhost:7257/api/Response/5");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Response service returned sucsess result ");
+                return Ok();
+            }
+            Console.WriteLine("Response service returned Fail result ");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+
+        }
+    }
+}
