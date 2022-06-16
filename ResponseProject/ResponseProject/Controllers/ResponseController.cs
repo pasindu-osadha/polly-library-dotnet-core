ce7623f8db69b7ec59466668740efa4fd589a1f2
pasindu-osadha <pasiduosanda@gmail.com>
2022-06-16 14:52:04 +0530

    add basic response controller method

---
+++ ResponseProject/ResponseProject/Controllers/ResponseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResponseProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResponseController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult GetResponse(int id)
+        {
+            Random random = new Random();
+            int randomInteger = random.Next(1, 11);
+
+            if (randomInteger >= id)
+            {
+                Console.WriteLine("Resposnse Service Generate Internal server error HTTP 500");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            Console.WriteLine("Resposnse Service Generate Sucess HTTP 200");
+            return Ok();
+        }
+    }
+}
