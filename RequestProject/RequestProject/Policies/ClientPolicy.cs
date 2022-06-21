bb36ec3cd5d5abbf5fc67052ba71b942e418dedb
pasindu-osadha <pasiduosanda@gmail.com>
2022-06-21 15:55:14 +0530

    create immediate retry policy and configure

---
+++ RequestProject/RequestProject/Policies/ClientPolicy.cs
@@ -0,0 +1,14 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace RequestProject.Policies
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmediteHttpRetryPolicy { get;}
+        public ClientPolicy()
+        {
+            ImmediteHttpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(res=>!res.IsSuccessStatusCode).RetryAsync(5);
+        }
+    }
+}
